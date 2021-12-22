d53028f60d88683359585321ab635256394cde08
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2021-12-22 02:59:59 -0700

    Personel Controller ve Index

---
+++ OnlineTicariOtomasyon/Controllers/PersonelController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnlineTicariOtomasyon.Models.Sınıflar;
+
+namespace OnlineTicariOtomasyon.Controllers
+{
+    public class PersonelController : Controller
+    {
+        // GET: Personel
+        private Context context = new Context();
+        public ActionResult Index()
+        {
+            var degerler = context.Personels.ToList();
+            return View(degerler);
+        }
+    }
+}
