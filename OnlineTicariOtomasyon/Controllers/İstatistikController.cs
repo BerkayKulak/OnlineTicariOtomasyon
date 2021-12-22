39aec3ea07635421980ac47028a6a16bac382e57
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2021-12-22 13:07:02 -0700

    Linq Sorgularla İstatistik Çekme 2

--- OnlineTicariOtomasyon/Controllers/İstatistikController.cs
+++ OnlineTicariOtomasyon/Controllers/İstatistikController.cs
@@ -22,6 +22,25 @@ namespace OnlineTicariOtomasyon.Controllers
             ViewBag.d3 = deger3;
             var deger4 = c.Kategoris.Count().ToString();
             ViewBag.d4 = deger4;
+
+            // ürünlerdeki stok sayısının toplamı
+            var deger5 = c.Uruns.Sum(x=>x.Stok).ToString();
+            ViewBag.d5 = deger5;
+
+            //Kaç Çeşit Marka Var
+            var deger6 = (from x in c.Uruns select x.Marka).Distinct().Count();
+            ViewBag.d6 = deger6;
+
+            //Stok sayısı 20 nin altında olan ürün sayısı
+            var deger7 = c.Uruns.Count(x => x.Stok <= 20).ToString();
+            ViewBag.d7 = deger7;
+
+            // max fiyatlı ürün
+            var deger8 = (from x in c.Uruns orderby x.SatisFiyat descending select x.UrunAd).FirstOrDefault();
+            ViewBag.d8 = deger8;
+
+
+
             return View();
         }
     }
