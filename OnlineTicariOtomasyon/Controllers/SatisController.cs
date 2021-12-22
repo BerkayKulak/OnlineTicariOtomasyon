1da4b38b8cc74060f6aeca710604be76126e9f3c
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2021-12-22 04:14:09 -0700

    Satış Index

---
+++ OnlineTicariOtomasyon/Controllers/SatisController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnlineTicariOtomasyon.Models.Sınıflar;
+
+namespace OnlineTicariOtomasyon.Controllers
+{
+    public class SatisController : Controller
+    {
+        // GET: Satis
+        private Context context = new Context();
+        public ActionResult Index()
+        {
+            var degerler = context.SatisHarekets.ToList();
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public ActionResult YeniSatis()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult YeniSatis(SatisHareket satisHareket)
+        {
+            context.SatisHarekets.Add(satisHareket);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
