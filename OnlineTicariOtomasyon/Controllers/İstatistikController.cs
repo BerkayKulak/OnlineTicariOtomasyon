f5a9e94dfa0b3dafcbca46851cf9de38f51f0df4
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2021-12-22 12:58:46 -0700

    Linq Sorgularla İstatistik Çekme 1

--- OnlineTicariOtomasyon/Controllers/İstatistikController.cs
+++ OnlineTicariOtomasyon/Controllers/İstatistikController.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OnlineTicariOtomasyon.Models.Sınıflar;
 
 namespace OnlineTicariOtomasyon.Controllers
 {
     public class İstatistikController : Controller
     {
         // GET: İstatistik
+        private Context c = new Context();
         public ActionResult Index()
         {
+            // Carileri say
+            var deger1 = c.Carilers.Count().ToString();
+            ViewBag.d1 = deger1;
+            var deger2 = c.Uruns.Count().ToString();
+            ViewBag.d2 = deger2;
+            var deger3 = c.Personels.Count().ToString();
+            ViewBag.d3 = deger3;
+            var deger4 = c.Kategoris.Count().ToString();
+            ViewBag.d4 = deger4;
             return View();
         }
     }
