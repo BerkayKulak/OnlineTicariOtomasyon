1b74bbec7b2583cacbb2b4dcf25db1fe1dbe2b5f
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2021-12-26 13:30:06 -0700

    Bir Viewde Birden Fazla Tablo Listeleme & IEnumarable

--- OnlineTicariOtomasyon/Controllers/UrunDetayController.cs
+++ OnlineTicariOtomasyon/Controllers/UrunDetayController.cs
@@ -13,8 +13,11 @@ namespace OnlineTicariOtomasyon.Controllers
         private Context context = new Context();
         public ActionResult Index()
         {
-            var degerler = context.Uruns.Where(x => x.UrunId == 2).ToList();
-            return View(degerler);
+            UrunveDetay detay = new UrunveDetay();
+            //var degerler = context.Uruns.Where(x => x.UrunId == 2).ToList();
+            detay.Deger1 = context.Uruns.Where(x => x.UrunId == 2).ToList();
+            detay.Deger2 = context.Detays.Where(y => y.DetayID == 1).ToList();
+            return View(detay);
         }
     }
 }
