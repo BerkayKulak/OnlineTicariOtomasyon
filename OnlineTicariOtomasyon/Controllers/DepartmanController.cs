22942fca078c7472f011fca1ab5a595cf97a74e4
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2021-12-28 01:29:10 -0700

    Sayfalamada Sayfa Numaraları Getirme

--- OnlineTicariOtomasyon/Controllers/DepartmanController.cs
+++ OnlineTicariOtomasyon/Controllers/DepartmanController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using OnlineTicariOtomasyon.Models.Sınıflar;
+using PagedList;
 
 namespace OnlineTicariOtomasyon.Controllers
 {
@@ -11,9 +12,9 @@ namespace OnlineTicariOtomasyon.Controllers
     {
         // GET: Departman
         private Context context = new Context();
-        public ActionResult Index()
+        public ActionResult Index(int sayfa = 1)
         {
-            var degerler = context.Departmans.Where(x => x.Durum == true).ToList();
+            var degerler = context.Departmans.Where(x => x.Durum == true).ToList().ToPagedList(sayfa, 4);
             return View(degerler);
         }
 
