0af3b3663dfd971c1322da2aed77bde23ae9b366
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2021-12-29 00:26:13 -0700

    Grafiklere Veri Tabanından Veri Çekme

--- OnlineTicariOtomasyon/Controllers/GrafikController.cs
+++ OnlineTicariOtomasyon/Controllers/GrafikController.cs
@@ -1,15 +1,18 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Helpers;
 using System.Web.Mvc;
+using OnlineTicariOtomasyon.Models.Sınıflar;
 
 namespace OnlineTicariOtomasyon.Controllers
 {
     public class GrafikController : Controller
     {
         // GET: Grafik
+        private Context context = new Context();
         public ActionResult Index()
         {
             return View();
@@ -21,6 +24,18 @@ namespace OnlineTicariOtomasyon.Controllers
             grafikciz.AddTitle("Kategori - Ürün Stok Sayısı").AddLegend("Stok").AddSeries("Değerler",
                 xValue: new string[] {"Beyaz Eşya", "Telefon", "Küçük Ev Aleti"}, yValues: new int[] {85, 66, 98}).Write();
             return File(grafikciz.ToWebImage().GetBytes(),"image/jpeg");
+        }
+
+        public ActionResult Index3()
+        {
+            ArrayList xvalue = new ArrayList();
+            ArrayList yvalue = new ArrayList();
+            var sonuclar = context.Uruns.ToList();
+            sonuclar.ToList().ForEach(x=>xvalue.Add(x.UrunAd));
+            sonuclar.ToList().ForEach(y=>yvalue.Add(y.Stok));
+            var grafik = new Chart(width: 500, height: 500).AddTitle("Stoklar")
+                .AddSeries(chartType: "Column", name: "Stok", xValue: xvalue, yValues: yvalue);
+            return File(grafik.ToWebImage().GetBytes(), "image/jpeg");
         }
     }
 }
